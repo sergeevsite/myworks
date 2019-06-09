4268c30a9f9e4d45ba7ba6a4171a4fcc6d71de63
Andrey Sergeev <andreyinoks93@yandex.ru>
2019-06-09 17:46:45 +0700

    Установка счетчика метрики и цели на кнопку Перезвоните мне

---
+++ lesson-23/repair-hard/js/modal.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  // Модальное окно
+  var button = $('#button');
+  var modal = $('#modal');
+  var close = $('#close');
+
+  button.on('click', function() {
+    modal.addClass('modal_active');
+  });
+
+  close.on('click', function() {
+    modal.removeClass('modal_active');
+  });
+
+  // Кнопка наверх
+  var moveTop = $('#top');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+      moveTop.addClass('animated fadeInUp'),
+      moveTop.addClass('move-top__button_none');
+      moveTop.removeClass('fadeOut');
+    }else if ($(window).scrollTop() == 0) {
+        moveTop.removeClass('fadeInUp'),
+        moveTop.addClass('fadeOut');
+        setTimeout(function() {
+          moveTop.removeClass('move-top__button_none');
+        }, 800);
+    }else{
+
+    }
+  });
+
+  moveTop.on('click', function(){
+    $('body,html').animate({scrollTop:0},600);
+  });
+
+});
