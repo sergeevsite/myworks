4e1a4a097627b9c70602c5617b4f24a4844168df
Andrey Sergeev <andreyinoks93@yandex.ru>
2019-06-15 00:00:30 +0700

    Добавил 24 урок

---
+++ lesson-24/src/js/load-map.js
@@ -0,0 +1,11 @@
+/* Скрипт загрузки карты */
+
+var excellence = $('.excellence');
+var excellenceTop = excellence.offset().top;
+$(window).bind('scroll', function(){
+  var windowTop = $(this).scrollTop();
+  if (windowTop > excellenceTop) {
+    $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A32f5ea56de6cc60007c95e055523096ce3f8aed26291c6ee07ae1927dac57809&amp;width=100%25&amp;height=640&amp;lang=ru_RU&amp;scroll=true"></script>')
+    $(window).unbind('scroll');
+  }
+});
---
+++ lesson-24/src/js/map.js
@@ -0,0 +1,35 @@
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [55.61140907, 37.20112250],
+    zoom: 16
+  }),
+  myPieChart = new ymaps.Placemark([
+      55.61140907, 37.20112250
+  ], {
+    // Зададим произвольный макет метки.
+    iconLayout: 'default#pieChart',
+    // Радиус диаграммы в пикселях.
+    iconPieChartRadius: 30,
+    // Радиус центральной части макета.
+    iconPieChartCoreRadius: 10,
+    // Стиль заливки центральной части.
+    iconPieChartCoreFillStyle: '#ffffff',
+    // Cтиль линий-разделителей секторов и внешней обводки диаграммы.
+    iconPieChartStrokeStyle: '#ffffff',
+    // Ширина линий-разделителей секторов и внешней обводки диаграммы.
+    iconPieChartStrokeWidth: 3,
+    // Максимальная ширина подписи метки.
+    iconPieChartCaptionMaxWidth: 200
+  }); 
+    myMap.geoObjects
+      .add(myPieChart)
+      .add(new ymaps.Placemark([55.61140907, 37.20112250], {
+        balloonContent: 'г. Москва, ул. Ленина, д. 10, корпус 2, оф. 308'
+      }, {
+      preset: 'islands#icon',
+      iconColor: '#ffd200'
+      }));
+    myMap.behaviors.disable('ScrollZoom');
+};
+
